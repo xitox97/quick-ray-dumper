241c59da315772b1199b89c6d84fc662d6601461
Farhan Hadi <xitox97@gmail.com>
2022-05-10 22:06:31 +0800

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,19 @@ import * as vscode from "vscode"
 
 enum EAction {
     SHOW_QUERIES = "showQueries",
+    SHOW_QUERIES_CALLBACK = "showQueriesCallback",
     STOP_SHOWING_QUERIES = "stopShowingQueries",
 }
 
 export const activate = (context: vscode.ExtensionContext) => {
     context.subscriptions.push(
         vscode.commands.registerTextEditorCommand("quick-ray-dumper.showQueries", () => handleAction(EAction.SHOW_QUERIES)),
+        vscode.commands.registerTextEditorCommand("quick-ray-dumper.showQueriesCallback", () => handleActionCallback(EAction.SHOW_QUERIES_CALLBACK)),
         vscode.commands.registerTextEditorCommand("quick-ray-dumper.stopShowingQueries", () => handleAction(EAction.STOP_SHOWING_QUERIES))
     )
 }
 
-const handleAction = (action: string) => {
+const handleAction = (action: string,) => {
     const editor = vscode.window.activeTextEditor
 
     const getAction = (action: string): string => {
@@ -27,5 +29,17 @@ const handleAction = (action: string) => {
     if (editor) {
         const position = editor.selection.active
         editor.edit((builder) => builder.insert(position, getAction(action)))
+    }
+}
+
+const handleActionCallback = (action: string,) => {
+    const editor = vscode.window.activeTextEditor
+
+    if (editor) {
+        const position = editor.selection.active
+
+        if(action === EAction.SHOW_QUERIES_CALLBACK) {
+            // editor.edit((builder) => builder.insert(position, getAction(action)))
+        }
     }
 }
